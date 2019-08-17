116324a202a7c90aca372cb5825ad156117a88c0
zy.w530 <284141050@qq.com>
2019-08-18 01:14:39 +0800

    feat(all): init

---
+++ README.md
@@ -0,0 +1,2 @@
+# sharedsway/common
+
---
+++ src/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: debian
+ * Date: 19-8-18
+ * Time: 上午1:10
+ */
+namespace Sharedsway\Common;
+class Exception extends \Exception
+{
+
+}
